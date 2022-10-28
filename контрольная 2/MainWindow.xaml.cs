800b9ac731f5e43906fc4ca9cddf0ce79e6a1caf
Георгий Дудырин <gosha84gur@outlook.com>
2022-10-28 21:40:03 +0300

    Добавьте файлы проекта.

---
+++ контрольная 2/MainWindow.xaml.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Printing;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using Microsoft.VisualBasic;
+
+namespace контрольная_2
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+         Utilites utilites = new Utilites();
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+        
+
+        private void button1Zad_Click(object sender, RoutedEventArgs e)
+        {
+            int lenght = Convert.ToInt32(Interaction.InputBox("Введите длину массива"));
+            string[] Mass = new string[lenght];
+            Mass = utilites.completeMass(lenght);
+            string Repeat = utilites.RepeatingChar(Mass);
+        }
+    }
+}
---
+++ контрольная 2/Utilites.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection.Metadata;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Input.StylusPlugIns;
+using Microsoft.VisualBasic;
+namespace контрольная_2
+{
+    internal class Utilites
+    {
+        // Microsoft.VisualBasic.Interaction.InputBox(""); 
+        
+
+        public string[] completeMass(int lenght)//заполнение массива длиной lenght 
+        {
+            string[] str = new string[lenght];
+            for (int i = 0; i < str.Length; i++)
+            {
+                str[i] = Interaction.InputBox("Введите символ"); 
+            }
+            return str;
+        }
+        public string RepeatingChar(string[] chars)
+        {
+            
+            var count = 0;//количесвто повторов
+            var index = -1;//индекс в массиве
+            for (var i = 0; i < chars.Length; ++i)
+            {
+                var k = 1;
+                for (var j = i + 1; j < chars.Length; ++j)
+                    if (chars[i] == chars[j]) k++;
+                if (k <= count) continue;
+                count = k;
+                index = i;
+            }
+         
+            return chars[index]; //возвращает самое повторяющиеся число 
+        }
+    }
+}
